db9fcfc6e016fcb4ecf05f14c5a64552c946137f
Alex Spellanzon <alexspellanzon@gmail.com>
2020-05-23 22:47:42 +1000

    update static

--- fx_rates_project/settings.py
+++ fx_rates_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = ')3did^h14uxgl^fk@f5zlbvbg#z3f(vurkdejmau1pnz%n+r^)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*.pythonanywhere.com']
 
 
 # Application definition
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 INTERNAL_IPS = ['127.0.0.1']
 
 LOGIN_REDIRECT_URL = '/'
